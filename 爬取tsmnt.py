35ca3016d58901adb565f384295fcfd96847eb06
hbhz-official <85626391+hbhz-official@users.noreply.github.com>
2021-06-10 20:55:38 +0800

    Update 爬取tsmnt.py

--- 爬取tsmnt.py
+++ 爬取tsmnt.py
@@ -4,55 +4,97 @@ import urllib.request
 import random
 import threading
 import time
+import pymysql
+
+def 上传数据库(img):
+
+
+    sql="""INSERT INTO `img` (`url`, `style`) VALUES ('"""+img+"""', '');"""
+
+    try:
+
+       cursor.execute(sql)  
+       db.commit()
+       print('上传成功')
+
+    except:
+   # 如果发生错误则回滚
+
+        db.rollback()
+
+def 爬虫(url):
+    db = pymysql.connect(host='remotemysql.com', user="LKrNYglnPu",password='SJP4zwTBe0',database="LKrNYglnPu",charset='utf8')
+    cursor = db.cursor()
+    
+    
+    imgs=[]
+    r=requests.get(url)
+    html=r.text
+    sp=BeautifulSoup(html,"lxml")
+    for x in sp.find_all('a'):
+        if len(x.get('href'))<=17:
+            r=requests.get(url[0:50]+x.get('href'))
+            html=r.text
+            sp=BeautifulSoup(html,"lxml")
+            if sp.find('img') != None:
+                img= sp.find('img').get('src')
+                print(img)
+                sql="""INSERT INTO `img` (`url`, `style`) VALUES ('"""+img+"""', '');"""
+
+                try:
+
+                    cursor.execute(sql)  
+                    db.commit()
+                    print('上传成功')
+
+                except:
+   # 如果发生错误则回滚
+
+                    db.rollback()
+    db.close()         
+
+a=time.time()
 
 urls = []
-r=requests.get('http://www.xjjmzt.com/xiaojiejie/tuapicgm/25_1.html')
+r=requests.get('http://www.xjjmzt.com/xiaojiejie/tuapicgm/25_7.html')
 html=r.text
 sp=BeautifulSoup(html,"lxml")
 for x in sp.find_all('a'):
-    print(x.get('href'))
+    if x.get('href').find('xiaojiejie')>=0:
+        print(x.get('href'))
+        urls.append('http://www.xjjmzt.com'+x.get('href'))
+# 写法2
+# for url in urls:
+    
+    
+        # t=threading.Thread(target=爬虫,kwargs={'url':url})
+        # t.setDaemon(True)
+        # t.start()
+        # print('线程启动')
+        # time.sleep(5)
 
 
-
-
-    
-    
-
-
-    
-while(n<sta+1):
-    
-    u0="http://nsfwpicx.com"
-    
-    
-    
-    u1=u0+'/'+str(n)+'.html'
-    
-    
-    
-
-    r=requests.get(u1)
-    
-    
-    html=r.text
-    sp=BeautifulSoup(html,"lxml")
-
-    n=n+1
-    
-    
-    for x in sp.find_all('img'):
-         
-
-        print(x.get('src'))
-        urls.append(x.get('src'))
-        
-print(len(urls),'number') 
-a=time.time()
-
+# 写法1
 for url in urls:
-    t = threading.Thread(target=dpic,args=(url,))
-    t.setDaemon(True)
-    t.start()
-
+    thread_num = len(threading.enumerate())
+    while thread_num < 9 :
+        t=threading.Thread(target=爬虫,kwargs={'url':url})
+        t.setDaemon(True)
+        t.start()
+        print('线程启动')
+        break
+    else:
+        continue   
+        
+t.join()
 b=time.time()
-print(b-a)
+
+print('上传完成！')
+print(len(urls))
+print(b-a)  
+    
+
+
+    
+
+
