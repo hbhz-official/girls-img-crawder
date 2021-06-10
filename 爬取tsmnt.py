14ed6c0bc48e654e58c47ff0d1c3cef55ec33c13
hbhz-official <85626391+hbhz-official@users.noreply.github.com>
2021-06-10 21:10:07 +0800

    Update 爬取tsmnt.py

--- 爬取tsmnt.py
+++ 爬取tsmnt.py
@@ -46,23 +46,56 @@ def 爬虫(url):
                     cursor.execute(sql)  
                     db.commit()
                     print('上传成功')
+                    total = total+1
 
                 except:
-   # 如果发生错误则回滚
+   # 如z果发生错误则回滚
 
                     db.rollback()
     db.close()         
 
-a=time.time()
+total=0
 
-urls = []
-r=requests.get('http://www.xjjmzt.com/xiaojiejie/tuapicgm/25_7.html')
-html=r.text
-sp=BeautifulSoup(html,"lxml")
-for x in sp.find_all('a'):
-    if x.get('href').find('xiaojiejie')>=0:
-        print(x.get('href'))
-        urls.append('http://www.xjjmzt.com'+x.get('href'))
+
+for i in range (12,21):
+    a=time.time()
+
+
+    urls = []
+    r=requests.get('http://www.xjjmzt.com/xiaojiejie/tuapicgm/25_'+str(i)+'.html')
+    html=r.text
+    sp=BeautifulSoup(html,"lxml")
+    for x in sp.find_all('a'):
+        if x.get('href').find('xiaojiejie')>=0:
+            print(x.get('href'))
+            urls.append('http://www.xjjmzt.com'+x.get('href'))
+
+
+
+# 写法1
+    for url in urls:
+        thread_num = len(threading.enumerate())
+        while thread_num < 9 :
+            t=threading.Thread(target=爬虫,kwargs={'url':url})
+            t.setDaemon(True)
+            t.start()
+            print('线程启动')
+            break
+        else:
+            continue   
+        
+    t.join()
+    b=time.time()
+
+    print('上传完成！')
+    print(len(urls))
+    print(b-a)  
+    
+print('已上传：'+str(total))
+
+
+    
+
 # 写法2
 # for url in urls:
     
@@ -72,29 +105,6 @@ for x in sp.find_all('a'):
         # t.start()
         # print('线程启动')
         # time.sleep(5)
-
-
-# 写法1
-for url in urls:
-    thread_num = len(threading.enumerate())
-    while thread_num < 9 :
-        t=threading.Thread(target=爬虫,kwargs={'url':url})
-        t.setDaemon(True)
-        t.start()
-        print('线程启动')
-        break
-    else:
-        continue   
-        
-t.join()
-b=time.time()
-
-print('上传完成！')
-print(len(urls))
-print(b-a)  
-    
-
-
     
 
 
